b31a2acc22fb9ab582e583a728400f24376c6f7d
Sebastião Sukuakueche <sukuakueches@gmail.com>
2026-03-20 17:38:49 +0100

    feat: removed title and update size flag

--- data.js
+++ data.js
@@ -2,18 +2,16 @@ export const translations = {
     pt: {
         'app-title': 'Câmara do Comércio e Indústria - Angola & Botswana',
         'coming-soon': 'Em breve',
-        'website-under-development': 'Site está em desenvolvimento',
         'cci': 'Câmara de Comércio e Indústria',
-        'in-production': 'Em produção',
+        'in-construction': 'Em Construção',
         'building-something-remarkable': 'A construir algo grande',
         'language': 'Idioma',
     }, 
     en: {
         'app-title': 'Chamber of Commerce and Industry - Angola & Botswana',
         'coming-soon': 'Coming Soon',
-        'website-under-development': 'Website is under development',
         'cci': 'Chamber of Commerce and Industry',
-        'in-production': 'In Production',
+        'in-construction': 'In Construction',
         'building-something-remarkable': 'Building something remarkable',
         'language': 'Language',
     }
