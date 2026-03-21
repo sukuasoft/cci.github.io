f716e1ba3a1565bed0999ad8f9b0a2c39e774909
Sebastião Sukuakueche <sukuakueches@gmail.com>
2026-03-21 09:32:42 +0100

    feat: updated title, and text center

--- data.js
+++ data.js
@@ -1,18 +1,18 @@
 export const translations = {
     pt: {
-        'app-title': 'Câmara de Comércio e Indústria Angola - Botswana',
+        'app-title': 'Angola Botswana Câmara de Comércio e Indústria',
         'coming-soon': 'Em breve',
         'cci': 'Câmara de Comércio e Indústria',
         'in-construction': 'Em Construção',
-        'building-something-remarkable': 'A construir algo grande',
+        'tagline': '"Ligar mercados, Integrar a região, Expandir Visões"',
         'language': 'Idioma',
     }, 
     en: {
-        'app-title': 'Chamber of Commerce and Industry Angola - Botswana',
+        'app-title': 'Angola Botswana Chamber of Commerce and Industry',
         'coming-soon': 'Coming Soon',
         'cci': 'Chamber of Commerce and Industry',
         'in-construction': 'In Construction',
-        'building-something-remarkable': 'Building something remarkable',
+        'tagline': '"Connecting Markets, Integrating the Region, Expanding Visions"',
         'language': 'Language',
     }
 }
